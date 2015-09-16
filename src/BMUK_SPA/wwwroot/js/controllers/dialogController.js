14495a74c06686151f7959f6fe951b50c248a7bc
hitesh97 <khatri.hitesh@gmail.com>
2015-09-16 23:33:11 +0100

    member details showing in dialog box

---
+++ src/BMUK_SPA/wwwroot/js/controllers/dialogController.js
@@ -0,0 +1,13 @@
+﻿function DialogController($scope, $mdDialog, memberInfo) {
+    $scope.memberInfo = memberInfo;
+
+    $scope.hide = function () {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+        $mdDialog.hide(answer);
+    };
+};
